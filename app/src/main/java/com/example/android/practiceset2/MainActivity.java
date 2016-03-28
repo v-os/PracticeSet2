5a4f4e56989df3a41e3e637adb6a93780af87b7f
v-os <voidef@gmail.com>
2016-03-28 16:31:14 +0300

    Shopping List 1

--- app/src/main/java/com/example/android/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiceset2/MainActivity.java
@@ -11,11 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // PASTE CODE YOU WANT TO TEST HERE
-
-        int day1 = 15;
-        int day2 = 22;
-        int day3 = 18;
-        display(day1 + day2 + day3 / 3);
+        int raspberryPrice = 5;
+        display1("1 box: $" + raspberryPrice);
+        display2("2 boxes: $" + (raspberryPrice * 2));
+        display3("3 boxes: $" + (raspberryPrice * 3));
 
     }
 
